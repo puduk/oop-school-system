5b1f9ae7b66fee320553851007b8c19a42a97c06
Mert Demir <demirmert986@gmail.com>
2025-04-12 02:41:09 +0200

    Add management.hpp

---
+++ Utils/first_page.hpp
@@ -0,0 +1,7 @@
+#ifndef FIRST_PAGE_H
+#define FIRST_PAGE_H
+#include "student.hpp"
+
+int first_page(Student &s);
+
+#endif
---
+++ Utils/management.hpp
@@ -0,0 +1,8 @@
+#ifndef MANAGEMENT_H
+#define MANAGEMENT_H
+#include "student.hpp"
+
+
+int management(Student &s);
+
+#endif
